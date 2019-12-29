7c8ac4048c64a31555e8e4f9efa2119a4bec3a70
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 21:59:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "parakeet"
+  end
+  
+  get "/" do 
+    erb: index
+  end
+  
+  post "/checkout" do
+    session[:item] = params["item"]
+    #session["name"] = "Victoria"
+    @session = session
+    
+    erb :checkout
+  end 
+  
 end
